44ba2a3a541689127780d20c2a67874b6549bfc2
Miguel Oliveira <55458349+Miguel-Oliveiraa@users.noreply.github.com>
2024-03-25 23:37:56 -0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -47,6 +47,40 @@ sudo snap install postman
 #- discord
 sudo snap install discord
 
+#- Docker Engine
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
+
+#- Docker Desktop
+#kvm virtualizando
+modprobe kvm
+
+modprobe kvm_amd # Processador amd
+
+lsmod | grep kvm
+
+ls -al /dev/kvm
+
+sudo usermod -aG kvm $USER
+
+#Instalador docker desktop
+#https://docs.docker.com/desktop/install/ubuntu/
+
 #- gnome extensions
 sudo apt install gnome-shell-extensions
 # https://extensions.gnome.org/extension/3396/color-picker/
